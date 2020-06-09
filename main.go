05af4a729671e379b7338f8b69d4e2197a5d8fd4
GH Ryu <gh9503@gmail.com>
2020-06-09 19:17:09 +0900

    func multi string and multi return

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
+//3
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
+
+//2
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+//1
 func multiply(a, b int) int {
 	return a * b
 }
 
 func main() {
-	fmt.Println(multiply(2, 2))
+	// fmt.Println(multiply(2, 2))
+	// totalLength, upperName := lenAndUpper("nico")
+	// fmt.Println(totalLength, upperName)
+	repeatMe("nico", "lynn", "dal", "marl", "flynn")
 }
