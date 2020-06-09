ce0d45a1ff2fb7a64ffd7942bd6b3de5caa15898
GH Ryu <gh9503@gmail.com>
2020-06-09 22:26:50 +0900

    array & slice append

--- main.go
+++ main.go
@@ -3,11 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	a := 2
-	b := &a
-	c := &b
-	a = 5
-	fmt.Println(a, *b, **c)
-	*b = 10
-	fmt.Println(a, *b, **c)
+	names := []string{"nico", "lynn", "dal"}
+	names = append(names, "flynn")
+	fmt.Println(names)
 }
