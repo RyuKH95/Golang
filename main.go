92fe77566806a332631bbdb74884401ad1693152
GH Ryu <gh9503@gmail.com>
2020-06-10 18:05:49 +0900

    dictionary search add

--- main.go
+++ main.go
@@ -10,11 +10,24 @@ func main() {
 	// dictionary := mydict.Dictionary{}
 	// dictionary["hello"] = "hello"
 	// fmt.Println(dictionary)
-	dictionary := mydict.Dictionary{"first": "First word"}
-	definition, err := dictionary.Search("first")
+	// dictionary := mydict.Dictionary{"first": "First word"}
+	// definition, err := dictionary.Search("first")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println(definition)
+	// }
+	dictionary := mydict.Dictionary{}
+	word := "hello"
+	definition := "Greeting"
+	err := dictionary.Add(word, definition)
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
+	}
+	hello, _ := dictionary.Search("hello")
+	fmt.Println("found:", word, "definition:", hello)
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
 	}
 }
